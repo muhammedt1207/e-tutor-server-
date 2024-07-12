a2a19da6230681108bf636116b779e6e4488a892
muhammedt1207 <muhammedt127@gmail.com>
2024-07-12 20:46:45 +0530

    reafactor:routes-user

--- auth-service/src/presentation/index.ts
+++ auth-service/src/presentation/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(morgan('dev'))
 
 app.use('/api/auth',router(dependancies))
-// app.use('/',router(dependancies))
+app.use('/',router(dependancies))
 
 
 app.use("*",(req: Request, res: Response) => {
