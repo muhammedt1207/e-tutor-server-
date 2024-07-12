a52d733e89995cca1c040a9a8d34953b711f8621
muhammedt1207 <muhammedt127@gmail.com>
2024-07-12 11:11:41 +0530

    fix:ingress folder

--- auth-service/src/presentation/index.ts
+++ auth-service/src/presentation/index.ts
@@ -17,10 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(morgan('dev'))
 
-app.use('/',(req:Request,res:Response)=>{
-  
-  res.send('hello')
-})
+app.use('/',router(dependancies))
 app.use('/api/auth',router(dependancies))
 
 
